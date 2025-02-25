6cf951b8dd4b3ba88657e898313f227a832ef58b
candelasanzm <sanzmartincandela@gmail.com>
2025-02-25 12:09:58 +0100

    ej 6 hay que corregirlo

--- Ejercicio6_voraz.py
+++ Ejercicio6_voraz.py
@@ -6,21 +6,31 @@ def ordena_reservas(reservas : list[tuple[int, int]]) -> list[tuple[int, int]] :
     inicio = lambda reserva: reserva[0] # defino la funcion lambda para obtener la hora de inicio de cada reserva
     return sorted(reservas, key = inicio)
 
-def buscar_pista_libre(reservas : list[tuple[int, int]], hora_inicio : int, hora_fin : int) -> list[tuple[int, int]]:
+def buscar_pista_libre(reservas : list[tuple[int, int]]) -> list[tuple[int, int]]:
     """ Busca una pista libre, ordenando por hora de inicio las reservas """
     reservas_ordenadas = ordena_reservas(reservas)
-    pista_libre = []
-    actual = hora_inicio # variable que se va actualizando en funcion de la hora de fin de cada reserva
+    pistas = []
 
     for reserva in reservas_ordenadas:
-        if reserva[0] >= actual and reserva[1] <= hora_fin: # si la hora de inicio de la reserva es mayor o igual a la guardada en la variable actual y menor o igual a la hora de fin
-            pista_libre.append(reserva) # añadimos la reserva a la lista de pistas libres
-            actual = reserva[1] # actualizo la hora actual con la hora final de la reserva
+        asignado = False
 
-    return pista_libre
+        for pista in pistas:
+            if pista[-1][1] <= reserva[0]: # si la ultima reserva no coincide con la nueva reserva
+                pista.append(reserva)
+                asignado = True
+                break
+
+        if asignado == False:
+            pistas.append(reserva) # crear nueva pista
+
+    return pistas
+
+def calcular_pistas_necesarias(reservas : list[tuple[int, int]]) -> int :
+    pistas_libres = buscar_pista_libre(reservas)
+    return len(pistas_libres)
+
 
 res = [(10, 12), (9, 11), (11, 13), (13, 15)]
 print(ordena_reservas(res))
-hora_inicio = 9
-hora_fin = 15
-print(buscar_pista_libre(res, hora_inicio, hora_fin))
+print(buscar_pista_libre(res))
+print(calcular_pistas_necesarias(res))
